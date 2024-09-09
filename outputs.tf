ad7969c100127e61b3c1f2d1d8206812080eaae6
Joe <joe@fluidnumerics.com>
2024-09-09 13:53:01 -0400

    Set filesystem name to `/default`
    This is consistent with the filesystem that WEKA provisions
    automatically upon deployment

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,7 @@ output "weka_deployment_output" {
 output "network_storage" {
   description = "export of all desired folder directories"
   value = [{
-    remote_mount          = "/${var.default_disk_name}"
+    remote_mount          = "/default"
     local_mount           = var.local_mount
     fs_type               = "wekafs"
     mount_options         = "net=udp,remove_after_secs=900"
