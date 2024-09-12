a9124404efaf5118f9a09f1c14e235b331b32ae7
Joe <joe@fluidnumerics.com>
2024-09-12 16:33:04 -0400

    Add debug output

--- scripts/terminate_weka_cluster.sh
+++ scripts/terminate_weka_cluster.sh
@@ -6,8 +6,13 @@
 # can be used. Here, the `input` field is set to the terminate uri and the
 # cluster name separated by a `|` .
 
+set -x
+
 TERMINATE_CLUSTER_URI=$(echo $INPUT | awk -F "|" '{print $1}')
 CLUSTER_NAME=$(echo $INPUT | awk -F "|" '{print $2}')
+
+echo "terminate uri : $TERMINATE_CLUSTER_URI"
+echo "cluster name  : $CLUSTER_NAME"
 
 curl -m 70 -X POST "${TERMINATE_CLUSTER_URI}" \
 -H "Authorization:bearer $(gcloud auth print-identity-token)" \
