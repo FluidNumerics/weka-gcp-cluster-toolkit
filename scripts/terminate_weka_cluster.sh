e34921f52823b08ffa650b25a210b87af1fbe0a6
Joe <joe@fluidnumerics.com>
2024-09-13 10:05:10 -0400

    Remove excess escape character from curly bracket

--- scripts/terminate_weka_cluster.sh
+++ scripts/terminate_weka_cluster.sh
@@ -17,4 +17,4 @@ echo "cluster name  : $CLUSTER_NAME"
 curl -m 70 -X POST "${TERMINATE_CLUSTER_URI}" \
 -H "Authorization:bearer $(gcloud auth print-identity-token)" \
 -H "Content-Type:application/json" \
--d "\{\"name\":\"$CLUSTER_NAME\"\}"
+-d "{\"name\":\"$CLUSTER_NAME\"}"
